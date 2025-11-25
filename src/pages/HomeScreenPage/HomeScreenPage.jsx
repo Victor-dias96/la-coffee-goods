1255eae1c5b202c07fca37b1b572bae4c45ae73b
Victor-dias96 <diasv3882@gmail.com>
2025-11-25 19:09:37 +0000

    fix/bugs

--- src/pages/HomeScreenPage/HomeScreenPage.jsx
+++ src/pages/HomeScreenPage/HomeScreenPage.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import "./HomeScreenPage.css";
 
 function HomeScreenPage() {
-    return (
-        <div className="home-screen-page">
-            <Header />
-            <div className="button-row">
-                <div className="button-cadastro">
-                    <Button variant="primary" href="/register">ENTRAR COM CADASTRAR</Button>
-                </div>
+  const navigate = useNavigate();
 
-                <div className="button-sem-cadastro">
-                    <Button variant="secondary" href="/login">ENTRAR SEM CADASTRO</Button>
-                </div>
-            </div>
+  return (
+    <div className="home-screen-page">
+      <Header />
+      <div className="button-row">
+        <div className="button-cadastro">
+          <Button variant="primary" onClick={() => navigate("/register")}>
+            ENTRAR COM CADASTRAR
+          </Button>
         </div>
-    );
+
+        <div className="button-sem-cadastro">
+          <Button variant="secondary" onClick={() => navigate("/login")}>
+            ENTRAR SEM CADASTRO
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeScreenPage;
